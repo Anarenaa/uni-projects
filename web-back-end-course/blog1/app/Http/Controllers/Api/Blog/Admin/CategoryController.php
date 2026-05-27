faa530e42ad6199b8d5872b3b2e15038369ab5aa
Anastasiia Mikulych <mika.akima.22@gmail.com>
2026-05-27 23:22:19 +0300

    Add admin category controller

---
+++ web-back-end-course/blog1/app/Http/Controllers/Api/Blog/Admin/CategoryController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Api\Blog\Admin;
+
+//use App\Http\Controllers\Controller;
+use App\Models\BlogCategory;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+
+class CategoryController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //dd(__METHOD__);
+
+        $paginator = BlogCategory::paginate(5);
+ 
+        return $paginator;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //dd(__METHOD__);
+
+        $data = $request->all();
+
+        // Генерація slug, якщо користувач його не заповнив
+        if (empty($data['slug'])) {
+            $data['slug'] = Str::slug($data['title']);
+        }
+
+        // Створюємо об'єкт у базі даних
+        $item = BlogCategory::create($data);
+
+        if ($item) {
+            return [
+                'success' => true,
+                'message' => 'Успішно збережено',
+            ];
+        } else {
+            return [
+                'success' => false,
+                'message' => 'Помилка збереження',
+            ];
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //dd(__METHOD__);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //dd(__METHOD__);
+
+         $item = BlogCategory::find($id);
+        if (empty($item)) { //якщо ід не знайдено
+            return ['message' => "Запис id=[{$id}] не знайдено"];
+        }
+
+        $data = $request->all(); //отримаємо масив даних, які надійшли з форми
+        if (empty($data['slug'])) { //якщо псевдонім порожній
+            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
+        }
+ 
+        $result = $item->update($data);  //оновлюємо дані об'єкта і зберігаємо в БД
+
+        if ($result) {
+            return [
+            'success' => true,
+            'message' => 'Успішно збережено'
+            ];
+        } else {
+            return ['message' => 'Помилка збереження'];
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //dd(__METHOD__);
+    }
+}
